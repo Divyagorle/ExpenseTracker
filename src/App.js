2a553c1264c3e8e2db173bcc46f40a27c5dc5717
VIJAYAVADDI <vijayalakshmiv450@gmail.com>
2024-07-21 19:27:57 +0530

    Add authenticated and unauthenticated layouts and update login functionality

--- src/App.js
+++ src/App.js
@@ -3,27 +3,23 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import Login from './components/Admin/Login';
 import Signup from './components/Admin/Signup';
-import Header from './components/Header';
 import Dashboard from './components/Dashboard';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import Sidebar from './components/Sidebar';
 import Expense from './components/Expense';
 import Category from './components/Category';
-import PrivateRoute from './PrivateRoute';
-
+import AuthenticatedLayout from './AuthenticatedLayout';
 function App() {
   return (
     <AuthProvider>
       <Router>
-        <Header />
-        <Sidebar />
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/dashboard" element={<Dashboard/>} />
-          <Route path="/expenses" element={<Expense/>} />
-          <Route path="/category" element={<Category/>} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/expenses" element={<Expense />} />
+          <Route path="/category" element={<Category />} />
+          <Route path="/al" element={<AuthenticatedLayout/>} />
         </Routes>
       </Router>
     </AuthProvider>
